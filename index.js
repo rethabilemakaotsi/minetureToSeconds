96debd8f3f7e83767635234135e16714612c96ce
rethabile <rethabilemakaotsi17@gmail.com>
2023-11-22 10:12:35 +0200

    updates

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 function validateInput(minutes) {
-    // Validate input and return true if valid, false otherwise
+    
     return !isNaN(minutes) && minutes >= 0;
 }
 
-// Function for Seconds
+
 function convertToSeconds(minutes) {
-    // Convert minutes to seconds
+    
     return minutes * 60;
 }
 
-// Function writes answer to DOM
+
 function displayResult(seconds) {
-    // Display the result in the DOM
+    
     document.getElementById('result').textContent = `Result: ${seconds} seconds`;
 }
 
-// Main conversion function
+
 function convertMinutesToSeconds() {
-    // Get input value
+
     const minutesInput = document.getElementById('minutesInput').value;
 
-    // Input validation
+
     if (!validateInput(minutesInput)) {
         alert('Invalid input. Please enter a valid non-negative number.');
-        return; // exit function if input is invalid
+        return; 
     }
 
-    // Convert to seconds
+    
     const seconds = convertToSeconds(parseFloat(minutesInput));
 
-    // Output formatted to specific decimal places(2)
+    
     displayResult(seconds.toFixed(2));
 }
 
-// Your comments go here
